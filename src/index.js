8ff78643b01b4d221129ef00ca1c35069112a704
norikog3 <noriko.garratt@gmail.com>
2025-01-23 17:59:40 +0000

    error corrected

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.ariaValueMax;
+  cityElement.innerHTML = searchInput.value;
 }
 let searhFormElement = document.querySelector("#search-form");
 searhFormElement.addEventListener("submit", handleSearchSubmit);
