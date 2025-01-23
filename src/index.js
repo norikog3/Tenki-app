cdb3b2acdac4cec4fc9f974c216afe21a09561b6
norikog3 <noriko.garratt@gmail.com>
2025-01-23 14:21:32 +0000

    index.js added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = searchInput.ariaValueMax;
+}
+let searhFormElement = document.querySelector("#search-form");
+searhFormElement.addEventListener("submit", handleSearchSubmit);
