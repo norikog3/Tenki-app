42d328bad54c5a80c25a234dc62eaefaaf7cffa5
norikog3 <noriko.garratt@gmail.com>
2025-04-22 12:27:10 +0100

    corrected errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+function refreshElement(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temprature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "0fb423d44d6ba3f282ct0c23foeafd77";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric";
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 let searhFormElement = document.querySelector("#search-form");
