88abb3d94155bc6df39d51c70f04fa6b0ad9c49e
norikog3 <noriko.garratt@gmail.com>
2025-04-23 15:44:46 +0100

    more adjustments from lesson7

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-function refreshElement(response) {
+function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temprature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "0fb423d44d6ba3f282ct0c23foeafd77";
   let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric";
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -18,6 +20,9 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searhFormElement = document.querySelector("#search-form");
 searhFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Tokyo");
